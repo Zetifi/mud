53b984fb5e9a2bf144394af4c1c452d22a329453
ramon-git <rmn.ramon@zoho.com>
2022-12-16 15:34:47 +1100

    update mud header

--- mud.h
+++ mud.h
@@ -56,7 +56,7 @@ struct mud_path_conf {
     unsigned char pref;
     unsigned char fixed_rate;
     unsigned char loss_limit;
-    unsigned char max_rtt_limit;
+    uint64_t max_rtt_limit;
 };
 
 struct mud_path {
